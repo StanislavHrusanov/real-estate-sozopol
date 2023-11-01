52b460b75ae6cb6ef24375fd2e423d4e4d20b3df
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-01 13:10:24 +0200

    Add functionallity to connect to database and start the app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const { initializeDatabase } = require('./config/database');
+const { PORT } = require('./config/env');
+
+const app = express();
+
+require('./config/handlebars')(app);
+
+initializeDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`App is listening on port ${PORT}...`));
+    })
+    .catch((err) => {
+        console.log(`App cannot connect to database`, err);
+    });
