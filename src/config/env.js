1ecaaef41deb93cf156a9dc3ce256aa2740c6f2d
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-02 16:53:37 +0200

    Add functionality to register users

--- src/config/env.js
+++ src/config/env.js
@@ -2,3 +2,4 @@ exports.PORT = 3000;
 exports.CONNECTION_STRING = 'mongodb://localhost:27017/realEstateSozopol';
 exports.SECRET = 'dafafsadfvcsFSD';
 exports.SALT_ROUNDS = 10;
+exports.SESSION_NAME = 'user';
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const { SESSION_NAME } = require('../config/env');
+const authService = require('../services/authService');
+
 
 router.get('/register', async (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+    console.log(userData);
+    try {
+        const createdUser = await authService.register(userData);
+
+        const token = await authService.createToken(createdUser);
+
+        res.cookie(SESSION_NAME, token, { httpOnly: true });
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/login', async (req, res) => {
