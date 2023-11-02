d48a641c7df6ef418a20624fc3dd096a074147a6
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-02 16:19:30 +0200

    Add authService with functions createToken and register

--- src/config/env.js
+++ src/config/env.js
@@ -1,2 +1,4 @@
 exports.PORT = 3000;
 exports.CONNECTION_STRING = 'mongodb://localhost:27017/realEstateSozopol';
+exports.SECRET = 'dafafsadfvcsFSD';
+exports.SALT_ROUNDS = 10;
---
+++ src/services/authService.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { jwtSign } = require('../utils/jwtPromises');
+const { SALT_ROUNDS, SECRET } = require('../config/env');
+
+exports.createToken = (user) => {
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email
+    }
+
+    const options = { expiresIn: '2d' }
+
+
+    return jwtSign(payload, SECRET, options);
+}
+
+exports.register = async ({ username, firstName, lastName, password, email }) => {
+
+    const hashedPassword = bcrypt.hash(password, SALT_ROUNDS);
+
+    const createdUser = User.create({
+        username,
+        firstName,
+        lastName,
+        password: hashedPassword,
+        email
+    });
+
+    return createdUser;
+}
