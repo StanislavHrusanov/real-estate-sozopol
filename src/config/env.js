6a44f6e04d0b606187b5ea076da158f486d89c00
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-07 15:46:38 +0200

    Add cloudinary config

--- src/config/env.js
+++ src/config/env.js
@@ -3,3 +3,6 @@ exports.CONNECTION_STRING = 'mongodb://localhost:27017/realEstateSozopol';
 exports.SECRET = 'dafafsadfvcsFSD';
 exports.SALT_ROUNDS = 10;
 exports.SESSION_NAME = 'user';
+exports.CLOUD_NAME = 'doyfjnyvo';
+exports.API_KEY = '787555939896624';
+exports.API_SECRET = 'fjtqYTC_W_XwuHDgrPBf5b0ceuk';
