2c7a7b90e31598b531d85ca8edf0521b521be913
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-08 16:33:48 +0200

    Add functions to upload images to cloudinary and create Ads to database

--- src/services/catalogService.js
+++ src/services/catalogService.js
@@ -1,3 +1,16 @@
 const Ad = require('../models/Ad');
 const cloudinary = require('../config/cloudinary');
 
+exports.uploadToCloudinary = async (files, folder) => {
+    const imagesData = [];
+
+    for (const file of files) {
+        const path = file.path;
+        const data = await cloudinary.uploader.upload(path, { folder });
+        imagesData.push({ url: data.url, public_id: data.public_id });
+    }
+
+    return imagesData;
+}
+
+exports.createAd = (ad) => Ad.create(ad);
