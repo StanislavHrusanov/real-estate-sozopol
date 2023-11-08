35009f49d0970969d42b1ecf4a165390e62956ed
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-08 16:35:59 +0200

    Add functionality to upload images to cloudinary and add Ads to database

--- src/controllers/catalogController.js
+++ src/controllers/catalogController.js
@@ -1,12 +1,35 @@
 const router = require('express').Router();
 const cloudinary = require('../config/cloudinary');
+const upload = require('../middlewares/upload');
+const catalogService = require('../services/catalogService');
 
 router.get('/add', async (req, res) => {
     res.render('catalog/add');
 });
 
-router.post('/add', async (req, res) => {
-    
-})
+router.post('/add', upload.array('images', 10), async (req, res) => {
+
+    const ad = {
+        type: req.body.type,
+        location: req.body.location,
+        price: Number(req.body.price),
+        area: Number(req.body.area),
+        floor: req.body.floor,
+        phoneNumber: req.body.phoneNumber,
+        info: req.body.info
+    };
+
+    try {
+        const imagesData = await catalogService.uploadToCloudinary(req.files, 'properties');
+        ad.images = imagesData;
+        ad.owner = req.user._id;
+        await catalogService.createAd(ad);
+        res.redirect('/');
+
+    } catch (error) {
+        res.render('catalog/add', { ad, error });
+    }
+
+});
 
 module.exports = router;
