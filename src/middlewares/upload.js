a81addc2706d9590ab1379171fbfb080e3156bac
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-11 13:50:54 +0200

    Update upload middleware to have two fields

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 const storage = multer.diskStorage({});
 const upload = multer({ storage: storage });
+const imgUpload = upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'images', maxCount: 8 }]);
 
-module.exports = upload;
+module.exports = imgUpload;
