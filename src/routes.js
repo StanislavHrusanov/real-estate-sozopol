c5ff93411b8acc6d268bd673af6c21c8ed01009d
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-23 18:32:45 +0200

    Set routes to use myProfileController on /myProfile route

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const routes = require('express').Router();
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
 const catalogController = require('./controllers/catalogController');
+const myProfileController = require('./controllers/myProfileController');
 
 routes.use('/', homeController);
 routes.use('/auth', authController);
 routes.use('/catalog', catalogController);
+routes.use('/myProfile', myProfileController);
 
 module.exports = routes;
