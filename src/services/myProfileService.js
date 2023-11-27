42dfc9f74da4953164682b1ce4f000938a2539ae
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-11-27 16:09:59 +0200

    Add function getUserDetails

--- src/services/myProfileService.js
+++ src/services/myProfileService.js
@@ -1,3 +1,6 @@
 const Ad = require('../models/Ad');
+const User = require('../models/User');
 
 exports.getMyAds = (userId) => Ad.find({ owner: { $in: userId } });
+
+exports.getUserDetails = (userId) => User.findById(userId).populate('favourites');
